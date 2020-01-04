e39f1475161567446dc89f962a7b807406474831
Lukas Kiss <lukaskiss222@gmail.com>
2020-01-04 01:03:45 +0100

    Removing little mistakes in main

--- main.py
+++ main.py
@@ -9,21 +9,28 @@ from agarEnv import AgarEnv
 
 WINDOW_SIZE = (580,580)
 IMAGE_SIZE = (120,120)
-NUM_FRAMES = 7000
-STEPS = 
+NUM_FRAMES = 3
+STEPS = 7000
 logname = 'DQN-' + datetime.now().strftime("%Y:%m:%d--%H:%M:%S") + '-steps:{}'.format(STEPS)
 
+env = None
 def main():
-    env = AgarEnv(WINDOW_SIZE, IMAGE_SIZE, NUM_FRAMES)
+    env = AgarEnv(WINDOW_SIZE, IMAGE_SIZE, NUM_FRAMES,
+            max_score=150)
     model = DQN(CnnPolicy, env, verbose = 1, 
             target_network_update_freq=500,
             prioritized_replay=True,
             tensorboard_log='logs/')
 
     model.learn(total_timesteps=STEPS, tb_log_name=logname)
-    model.save('models/'+ logname)
-
+    model.save('models/'+logname)
+    
+    env.close()
 
     
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    finally:
+        if env is not None:
+            env.close()
